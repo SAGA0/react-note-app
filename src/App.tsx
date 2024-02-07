09bf3a808419144f99c8586477ae78bc7201792b
Ramzan <murdagaliev95@gmail.com>
2024-02-07 14:47:47 +0300

    half of project

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { NewNote } from './NewNote'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useLocalStorage } from './useLocalStorage'
+import { useMemo } from 'react'
+import { v4 as uuidV4 } from 'uuid'
 
 export type Note = {
 	id: string
@@ -32,6 +34,24 @@ export type Tag = {
 function App() {
 	const [notes, setNotes] = useLocalStorage<RawNote[]>('NOTES', [])
 	const [tags, setTags] = useLocalStorage<Tag[]>('TAGS', [])
+
+	const notesWithTags = useMemo(() => {
+		return notes.map((note) => {
+			return {
+				...note,
+				tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+			}
+		})
+	}, [notes, tags])
+
+	function onCreateNote({ tags, ...data }: NoteDate) {
+		setNotes((prevNotes) => {
+			return [
+				...prevNotes,
+				{ ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+			]
+		})
+	}
 	return (
 		<Container className="my-4">
 			<Routes>
